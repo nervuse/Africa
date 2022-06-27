2342464cb3467906f66579d7f01d17314c33e434
Elena Mogilevich <nervuse@gmail.com>
2022-06-27 17:51:59 +0200

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by elena on 27.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
